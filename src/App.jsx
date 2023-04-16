3e548559552fa31312918b2ffad5188872d17d06
Aryan Swaroop <aryanswaroophere@gmail.com>
2023-04-16 16:01:02 +0530

    this is the navbar completed#

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import TopBar from './top';
+
+function App() {
+  return (
+    <div className="App">
+    <TopBar />
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ src/top.jsx
@@ -0,0 +1,11 @@
+export default function TopBar() {
+   return(
+    <div className="topBar" >
+        <div className="topbarElements" ></div>
+        <div className="topbarElements">
+            <img src="newlogo.png" className="logo" />
+        </div>
+        <div className="navbar-search-div"><input placeholder="search site" type="text"></input></div>
+    </div>
+   )
+} ; 
