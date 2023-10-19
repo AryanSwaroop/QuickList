5f49c3ffb6de31cd12fe7f292f299a6c0c2898cc
Aryan Swaroop <aryanswaroophere@gmail.com>
2023-10-19 09:22:46 +0530

    final phase 18

--- backend/app.js
+++ backend/app.js
@@ -31,7 +31,7 @@ async function main() {
 
 
 
-app.get("https://backend-portfolio-coral-eight.vercel.app",(req,res)=>{
+app.get("https://backend-portfolio-coral-eight.vercel.app/",(req,res)=>{
   res.send("Hello I am up!")
 })
 
@@ -230,6 +230,6 @@ passport.deserializeUser((user,done)=>{
 
 app.use("/auth", authRoute);
 
-app.listen(process.env.PORT,()=>{
+app.listen('https://backend-portfolio-coral-eight.vercel.app/',()=>{
     console.log("Server Running at port 5000");
 })
