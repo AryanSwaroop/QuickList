48d84e931dee172aa7714c6455cc5abb6a774bdc
Aryan Swaroop <aryanswaroophere@gmail.com>
2023-10-22 11:04:57 +0530

    final phase 48

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const passport = require("passport");
 
 
-router.get("/login/failed", (req,res)=>{
+router.get("https://backend-portfolio-igy4.onrender.com/login/failed", (req,res)=>{
     res.status(401).json({
         success: false,
         message: "failure",
@@ -14,7 +14,7 @@ router.get("/login/failed", (req,res)=>{
 
 
 
-router.get("/login/success", (req,res)=>{
+router.get("https://backend-portfolio-igy4.onrender.com/login/success", (req,res)=>{
     if (req.user) {
         res.status(200).json({
         success: true,
