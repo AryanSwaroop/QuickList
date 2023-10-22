8b8cec0eb18de0360f1b285a43021c86c9224264
Aryan Swaroop <aryanswaroophere@gmail.com>
2023-10-22 13:05:33 +0530

    final phase 54

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,14 +2,14 @@ const router = require("express").Router();
 const passport = require("passport");
 
 
-router.get("https://backend-portfolio-igy4.onrender.com/login/failed", (req,res)=>{
+router.get("/login/failed", (req,res)=>{
     res.status(401).json({
         success: false,
         message: "failure",
     })
 });
 
-router.get("https://backend-portfolio-igy4.onrender.com/login/success", (req,res)=>{
+router.get("/login/success", (req,res)=>{
     if (req.user) {
         res.status(200).json({
         success: true,
@@ -27,10 +27,10 @@ router.get("/logout", (req,res)=>{
 
 //router.get("/google/callback", passport.authenticate("google",{}));
 
-router.get("https://backend-portfolio-igy4.onrender.com/google/callback", passport.authenticate("google",{
+router.get("/google/callback", passport.authenticate("google",{
     scope:["profile"],
     successRedirect:"https://frontend-portfolio-aryan.vercel.app",
-    failureRedirect:"https://backend-portfolio-igy4.onrender.com/login/failed"
+    failureRedirect:"/login/failed"
 }));
 
 module.exports = router;
